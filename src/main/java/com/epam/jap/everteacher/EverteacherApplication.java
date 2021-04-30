fc9788ea8c355f6c2709372af5c2b89cb03000e2
pawel-korniak <pawello1020@gmail.com>
2021-04-30 13:13:57 +0200

    initial commit

---
+++ src/main/java/com/epam/jap/everteacher/EverteacherApplication.java
@@ -0,0 +1,13 @@
+package com.epam.jap.everteacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverteacherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverteacherApplication.class, args);
+	}
+
+}
