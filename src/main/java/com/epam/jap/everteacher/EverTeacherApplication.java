d1d0075d4d0d08c4c2b4cf8cec1ccb284165dd99
Veronika Avramchuk <Veronika_Avramchuk@epam.com>
2021-05-10 11:25:40 +0200

    encrypt database password and username using jasypt

--- src/main/java/com/epam/jap/everteacher/EverTeacherApplication.java
+++ src/main/java/com/epam/jap/everteacher/EverTeacherApplication.java
@@ -1,9 +1,11 @@
 package com.epam.jap.everteacher;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class EverTeacherApplication {
 
     public static void main(String[] args) {
